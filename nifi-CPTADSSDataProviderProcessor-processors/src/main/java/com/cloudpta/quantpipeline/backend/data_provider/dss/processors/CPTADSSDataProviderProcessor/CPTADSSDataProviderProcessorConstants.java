520e546446cc0783b0c597ba50b1783fc60998d1
dannyb2018 <39492136+dannyb2018@users.noreply.github.com>
2019-11-17 23:17:13 +0900

    added constants for datastream data requests

--- nifi-CPTADSSDataProviderProcessor-processors/src/main/java/com/cloudpta/quantpipeline/backend/data_provider/dss/processors/CPTADSSDataProviderProcessor/CPTADSSDataProviderProcessorConstants.java
+++ nifi-CPTADSSDataProviderProcessor-processors/src/main/java/com/cloudpta/quantpipeline/backend/data_provider/dss/processors/CPTADSSDataProviderProcessor/CPTADSSDataProviderProcessorConstants.java
@@ -42,10 +42,30 @@ public interface CPTADSSDataProviderProcessorConstants
     public static final String DSWS_PASSWORD_PROPERTY = "";
     public static final String DSWS_PASSWORD_PROPERTY_DISPLAY = "Datascope Password";
     public static final String DSWS_PASSWORD_PROPERTY_DESCRIPTION = "Password for Refinitiv Datascope";
-    public static final String DSWS_BASE_URL_PROPERTY = "";    
-    public static final String DSWS_BASE_URL_PROPERTY_DISPLAY = "";    
-    public static final String DSWS_BASE_URL_PROPERTY_DESCRIPTION = "";    
-    public static final String DSWS_BASE_URL_PROPERTY_DEFAULT_VALUE = "";    
+    public static final String DSWS_BASE_URL_PROPERTY = "DSWS_BASE_URL";    
+    public static final String DSWS_BASE_URL_PROPERTY_DISPLAY = "Datascope Base Url";    
+    public static final String DSWS_BASE_URL_PROPERTY_DESCRIPTION = "Base Url for all Datascope requests";    
+    public static final String DSWS_BASE_URL_PROPERTY_DEFAULT_VALUE = "http://product.datastream.com/DswsClient/V1/DSService.svc/rest";    
+    
+    public static final String DSWS_GET_TOKEN = "/GetToken";
+    public static final String DSWS_GET_DATA = "/GetData";
+    public static final String DSWS_TOKEN_VALUE_FIELD = "TokenValue";
+    public static final String DSWS_USER_NAME_FIELD = "UserName";
+    public static final String DSWS_PASSSWORD_FIELD = "Password";
+    public static final String DSWS_PROPERTIES_FIELD = "Properties";
+    public static final String DSWS_DATA_REQUEST_FIELD = "DataRequest";
+    public static final String DSWS_DATA_TYPES_FIELD = "DataTypes";
+    public static final String DSWS_DATE_FIELD = "Date";
+    public static final String DSWS_INSTRUMENT_FIELD = "Instrument";
+    public static final String DSWS_TAG_FIELD = "Tag";
+    public static final String DSWS_VALUE_FIELD = "Value";
+    public static final String DSWS_END_OFFSET_FIELD = "End";
+    public static final String DSWS_START_OFFSET_FIELD = "Start";
+    public static final String DSWS_KIND_FIELD = "Kind";
+    public static final String DSWS_FREQUEST_FIELD = "Frequency";
+    
+    public static final String DATA_RESPONSE_DATE_FORMAT = "yyyy-MM-dd";
+    
     
     public static final String RICS_ARRAY_NAME = "rics";
     public static final String FIELDS_ARRAY_NAME = "fields";
