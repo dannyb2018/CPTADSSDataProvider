037cc53b39ef19fd07656b5f4ef23aba2881045b
dannyb2018 <39492136+dannyb2018@users.noreply.github.com>
2019-12-11 00:56:17 +0900

    need to pass in a logger to the components to track what is happening

--- nifi-CPTADSSDataProviderProcessor-processors/src/main/java/com/cloudpta/quantpipeline/backend/data_provider/dss/processors/CPTADSSDataProviderProcessor/request_response/CPTARefinitivMessage.java
+++ nifi-CPTADSSDataProviderProcessor-processors/src/main/java/com/cloudpta/quantpipeline/backend/data_provider/dss/processors/CPTADSSDataProviderProcessor/request_response/CPTARefinitivMessage.java
@@ -22,6 +22,7 @@ package com.cloudpta.quantpipeline.backend.data_provider.dss.processors.CPTADSSD
 import com.cloudpta.quantpipeline.api.instrument.symbology.CPTAInstrumentSymbology;
 import java.util.List;
 import javax.json.JsonObject;
+import org.apache.nifi.logging.ComponentLog;
 import org.apache.nifi.processor.ProcessContext;
 
 /**
@@ -32,6 +33,7 @@ public abstract class CPTARefinitivMessage
 {
     public abstract JsonObject getResult
                                        (
+                                       ComponentLog logger,
                                        ProcessContext context,        
                                        List<CPTAInstrumentSymbology> symbols, 
                                        List<String> fields, 
