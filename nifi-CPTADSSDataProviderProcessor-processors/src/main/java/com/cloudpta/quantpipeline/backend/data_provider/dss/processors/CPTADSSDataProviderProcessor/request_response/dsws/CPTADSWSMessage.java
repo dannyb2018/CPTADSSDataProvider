922a0a71815db9129e172ad53c33a772ca1a4bb5
dannyb2018 <39492136+dannyb2018@users.noreply.github.com>
2019-12-11 00:57:30 +0900

    need to pass in a logger to the components to track what is happening

--- nifi-CPTADSSDataProviderProcessor-processors/src/main/java/com/cloudpta/quantpipeline/backend/data_provider/dss/processors/CPTADSSDataProviderProcessor/request_response/dsws/CPTADSWSMessage.java
+++ nifi-CPTADSSDataProviderProcessor-processors/src/main/java/com/cloudpta/quantpipeline/backend/data_provider/dss/processors/CPTADSSDataProviderProcessor/request_response/dsws/CPTADSWSMessage.java
@@ -44,6 +44,7 @@ import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import org.apache.nifi.logging.ComponentLog;
 import org.apache.nifi.processor.ProcessContext;
 
 /**
@@ -55,6 +56,7 @@ public class CPTADSWSMessage extends CPTARefinitivMessage
     @Override
     public JsonObject getResult
                               (
+                              ComponentLog logger, 
                               ProcessContext context, 
                               List<CPTAInstrumentSymbology> symbols, 
                               List<String> fields, 
