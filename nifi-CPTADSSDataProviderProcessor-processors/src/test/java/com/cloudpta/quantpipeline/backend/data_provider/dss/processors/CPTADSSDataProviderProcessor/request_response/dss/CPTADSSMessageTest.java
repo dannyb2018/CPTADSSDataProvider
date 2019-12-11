2790b0cc9442fa4d428972fb02403f0748d029fd
dannyb2018 <39492136+dannyb2018@users.noreply.github.com>
2019-12-11 23:00:27 +0900

    skeleton for test for dss super class message where most of the work is done

---
+++ nifi-CPTADSSDataProviderProcessor-processors/src/test/java/com/cloudpta/quantpipeline/backend/data_provider/dss/processors/CPTADSSDataProviderProcessor/request_response/dss/CPTADSSMessageTest.java
@@ -0,0 +1,107 @@
+/*
+
+Copyright 2017-2019 Advanced Products Limited, 
+dannyb@cloudpta.com
+github.com/dannyb2018
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+*/
+package com.cloudpta.quantpipeline.backend.data_provider.dss.processors.CPTADSSDataProviderProcessor.request_response.dss;
+
+import javax.json.JsonArray;
+import javax.json.JsonObjectBuilder;
+import javax.ws.rs.core.Response;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Danny
+ */
+public class CPTADSSMessageTest
+{
+    /**
+     * Test of getSessionToken method, of class CPTADSSMessage.
+     */
+    @Test
+    public void testGetSessionToken()
+    {
+        System.out.println("getSessionToken");
+        CPTADSSMessage instance = new CPTADSSMessage();
+    }
+
+    /**
+     * Test of getData method, of class CPTADSSMessage.
+     */
+    @Test
+    public void testGetData()
+    {
+        System.out.println("getData");
+        long timeout = 0L;
+        CPTADSSMessage instance = new CPTADSSMessage();
+    }
+
+    /**
+     * Test of handleError method, of class CPTADSSMessage.
+     */
+    @Test
+    public void testHandleError()
+    {
+        System.out.println("handleError");
+        Response response = null;
+        CPTADSSMessage instance = new CPTADSSMessage();
+    }
+
+    /**
+     * Test of buildExtractionRequest method, of class CPTADSSMessage.
+     */
+    @Test
+    public void testBuildExtractionRequest()
+    {
+        System.out.println("buildExtractionRequest");
+        CPTADSSMessage instance = new CPTADSSMessage();
+    }
+
+    /**
+     * Test of addConditions method, of class CPTADSSMessage.
+     */
+    @Test
+    public void testAddConditions()
+    {
+        System.out.println("addConditions");
+        JsonObjectBuilder detailsSpecificToThisExtractionRequest = null;
+        CPTADSSMessage instance = new CPTADSSMessage();
+    }
+
+    /**
+     * Test of getResult method, of class CPTADSSMessage.
+     */
+    @Test
+    public void testGetResult_Response()
+    {
+        System.out.println("getResult");
+        Response response = null;
+        CPTADSSMessage instance = new CPTADSSMessage();
+    }
+
+    /**
+     * Test of getMessageType method, of class CPTADSSMessage.
+     */
+    @Test
+    public void testGetMessageType()
+    {
+        System.out.println("getMessageType");
+        CPTADSSMessage instance = new CPTADSSMessage();
+    }    
+}
