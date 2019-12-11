cf8f141f089c753c1b4d9e1fa4b90b66f109b622
dannyb2018 <39492136+dannyb2018@users.noreply.github.com>
2019-12-11 20:27:58 +0900

    moved some of the dsws constants to their own folder added a couple of constants for the properties values

--- nifi-CPTADSSDataProviderProcessor-processors/src/main/java/com/cloudpta/quantpipeline/backend/data_provider/dss/processors/CPTADSSDataProviderProcessor/CPTADSSDataProviderProcessorConstants.java
+++ nifi-CPTADSSDataProviderProcessor-processors/src/main/java/com/cloudpta/quantpipeline/backend/data_provider/dss/processors/CPTADSSDataProviderProcessor/CPTADSSDataProviderProcessorConstants.java
@@ -47,42 +47,18 @@ public interface CPTADSSDataProviderProcessorConstants
     public static final String DSWS_BASE_URL_PROPERTY_DESCRIPTION = "Base Url for all Datascope requests";    
     public static final String DSWS_BASE_URL_PROPERTY_DEFAULT_VALUE = "http://product.datastream.com/DswsClient/V1/DSService.svc/rest";    
     
-    public static final String DSWS_GET_TOKEN = "/GetToken";
-    public static final String DSWS_GET_DATA = "/GetData";
-    
-    public static final String DSWS_TOKEN_VALUE_FIELD = "TokenValue";
-    public static final String DSWS_USER_NAME_FIELD = "UserName";
-    public static final String DSWS_PASSSWORD_FIELD = "Password";
-    public static final String DSWS_PROPERTIES_FIELD = "Properties";
-    public static final String DSWS_DATA_REQUEST_FIELD = "DataRequest";
-    public static final String DSWS_DATA_TYPES_FIELD = "DataTypes";
-    public static final String DSWS_DATE_FIELD = "Date";
-    public static final String DSWS_DATES_FIELD = "Dates";
-    public static final String DSWS_INSTRUMENT_FIELD = "Instrument";
-    public static final String DSWS_TAG_FIELD = "Tag";
-    public static final String DSWS_VALUE_FIELD = "Value";
-    public static final String DSWS_END_OFFSET_FIELD = "End";
-    public static final String DSWS_START_OFFSET_FIELD = "Start";
-    public static final String DSWS_KIND_FIELD = "Kind";
-    public static final int DSWS_KIND_FIELD_DEFAULT = 1;
-    public static final String DSWS_FREQUENCY_FIELD = "Frequency";
-    public static final String DSWS_DATA_TYPE_VALUES_FIELD = "DataTypeValues";
-    public static final String DSWS_DATA_TYPE_FIELD = "DataType";    
-    public static final String DSWS_SYMBOL_VALUES_FIELD = "SymbolValues";    
-    public static final String DSWS_SYMBOL_FIELD = "Symbol";    
-    public static final String DSWS_TYPE_FIELD = "Type";    
-    
-    public static final String DATA_RESPONSE_DATE_FORMAT = "yyyy-MM-dd";
-    
+    public static final String CPTA_DATE_FORMAT = "yyyy-MM-dd";
+   public static final String CPTA_DATE_TIME_FORMAT = "yyyy-MM-dd";
+
     public static final String CPTA_START_DATE_PROPERTY = "start";
     public static final String CPTA_END_DATE_PROPERTY = "end";
     public static final String CPTA_FREQUENCY_PROPERTY = "frequency";
-    public static final String DSWS_START_DATE_PROPERTY_DEFAULT = "-0D";
-    public static final String DSWS_END_DATE_PROPERTY_DEFAULT = "-0D";
-    public static final String DSWS_FREQUENCY_PROPERTY_DEFAULT = "D";
-    public static final String DSWS_FREQUENCY_PROPERTY_DAILY = "D";
-    public static final String DSWS_FREQUENCY_PROPERTY_WEEKLY = "W";
-    public static final String DSWS_FREQUENCY_PROPERTY_MONTHLY = "M";
+    public static final String CPTA_ADJUST_PRICES_PROPERTY = "adjust_prices";
+
+    public static final String CPTA_OFFSET_PROPERTY_DAILY = "D";
+    public static final String CPTA_OFFSET_PROPERTY_WEEKLY = "W";
+    public static final String CPTA_OFFSET_PROPERTY_MONTHLY = "M";        
+    public static final String CPTA_OFFSET_PROPERTY_YEARLY = "Y";        
     
     public static final String RICS_ARRAY_NAME = "rics";
     public static final String FIELDS_ARRAY_NAME = "fields";
