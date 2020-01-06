3a78dd00dacf5c95f0baa1bebf9749a96caa8845
dannyb2018 <39492136+dannyb2018@users.noreply.github.com>
2020-01-06 23:12:12 +0900

    first cut of test for processor

--- nifi-CPTADSSDataProviderProcessor-processors/src/test/java/com/cloudpta/quantpipeline/backend/data_provider/dss/processors/CPTADSSDataProviderProcessor/CPTADSSDataProviderProcessorTest.java
+++ nifi-CPTADSSDataProviderProcessor-processors/src/test/java/com/cloudpta/quantpipeline/backend/data_provider/dss/processors/CPTADSSDataProviderProcessor/CPTADSSDataProviderProcessorTest.java
@@ -19,24 +19,207 @@ limitations under the License.
 */
 package com.cloudpta.quantpipeline.backend.data_provider.dss.processors.CPTADSSDataProviderProcessor;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.List;
+import java.util.UUID;
+import org.apache.nifi.util.MockFlowFile;
 import org.apache.nifi.util.TestRunner;
 import org.apache.nifi.util.TestRunners;
-import org.junit.Before;
+import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
+public class CPTADSSDataProviderProcessorTest 
+{
+    @Test
+    public void testProcessorWithEmptyFlowFile() 
+    {        
+        // Mock the input file
+        // If it is empty then it needs to have rics, fields and properties all empty
+        String emptyRequestString = "{\""+ CPTADSSDataProviderProcessorConstants.INSTRUMENTS_ARRAY_NAME + "\":[], \""+ CPTADSSDataProviderProcessorConstants.FIELDS_ARRAY_NAME + "\":[], \""+ CPTADSSDataProviderProcessorConstants.PROPERTIES_ARRAY_NAME + "\":[]}";
+        InputStream content = new ByteArrayInputStream(emptyRequestString.getBytes());
 
-public class CPTADSSDataProviderProcessorTest {
+        // Generate a test runner to mock a processor in a flow
+        TestRunner runner = TestRunners.newTestRunner(new CPTADSSDataProviderProcessor());
 
-    private TestRunner testRunner;
+        // Set user names, passwords
+        runner.setProperty(CPTADSSDataProviderProcessorConstants.DSS_USER_NAME_PROPERTY, DSS_USER_NAME);
+        runner.setProperty(CPTADSSDataProviderProcessorConstants.DSWS_USER_NAME_PROPERTY, DSWS_USER_NAME);
+        runner.setProperty(CPTADSSDataProviderProcessorConstants.DSS_PASSWORD_PROPERTY, DSS_PASSWORD);
+        runner.setProperty(CPTADSSDataProviderProcessorConstants.DSWS_PASSWORD_PROPERTY, DSWS_PASSWORD);
 
-    @Before
-    public void init() {
-        testRunner = TestRunners.newTestRunner(CPTADSSDataProviderProcessor.class);
+        // Add the content to the runner
+        runner.enqueue(content);
+
+        // Run the enqueued content, it also takes an int = number of contents queued
+        runner.run(1);
+
+        // All results were processed with out failure
+        runner.assertQueueEmpty();
+
+        // If you need to read or do aditional tests on results you can access the content
+        List<MockFlowFile> results = runner.getFlowFilesForRelationship(CPTADSSDataProviderProcessorConstants.RELATIONSHIP_NAME_SUCCESS);
+        assertTrue(results.size() == 1);
+        MockFlowFile result = results.get(0);
+        String resultValue = new String(runner.getContentAsByteArray(result));
+
+        // Test attributes and content
+//        result.assertAttributeEquals(CPTADSSDataProviderProcessor.MATCH_ATTR, "nifi rocks");
+        result.assertContentEquals("nifi rocks");       
     }
 
     @Test
-    public void testProcessor() {
+    public void testProcessorWithWrongUserNamePassword() 
+    {        
+        // Mock the input file
+        // If it is empty then it needs to have rics, fields and properties all empty
+        String emptyRequestString = "{\""+ CPTADSSDataProviderProcessorConstants.INSTRUMENTS_ARRAY_NAME + "\":[{\"" + CPTADSSDataProviderProcessorConstants.IDENTIFIER_FIELD_NAME + "\": \"2618.TW\", \"" + CPTADSSDataProviderProcessorConstants.IDENTIFIER_TYPE_FIELD_NAME + "\": \"" + CPTADSSDataProviderProcessorConstants.IDENTIFIER_TYPE_RIC + "\"}], \""+ CPTADSSDataProviderProcessorConstants.FIELDS_ARRAY_NAME + "\":[], \""+ CPTADSSDataProviderProcessorConstants.PROPERTIES_ARRAY_NAME + "\":[]}";
+        InputStream content = new ByteArrayInputStream(emptyRequestString.getBytes());
 
+        // Generate a test runner to mock a processor in a flow
+        TestRunner runner = TestRunners.newTestRunner(new CPTADSSDataProviderProcessor());
+
+        // Set user names, passwords
+        runner.setProperty(CPTADSSDataProviderProcessorConstants.DSS_USER_NAME_PROPERTY, UUID.randomUUID().toString());
+        runner.setProperty(CPTADSSDataProviderProcessorConstants.DSWS_USER_NAME_PROPERTY, UUID.randomUUID().toString());
+        runner.setProperty(CPTADSSDataProviderProcessorConstants.DSS_PASSWORD_PROPERTY, UUID.randomUUID().toString());
+        runner.setProperty(CPTADSSDataProviderProcessorConstants.DSWS_PASSWORD_PROPERTY, UUID.randomUUID().toString());
+
+        // Add the content to the runner
+        runner.enqueue(content);
+
+        // Run the enqueued content, it also takes an int = number of contents queued
+        runner.run(1);
+
+        // All results were processed with out failure
+        runner.assertQueueEmpty();
+
+        // If you need to read or do aditional tests on results you can access the content
+        List<MockFlowFile> results = runner.getFlowFilesForRelationship(CPTADSSDataProviderProcessorConstants.RELATIONSHIP_NAME_SUCCESS);
+        assertTrue(results.size() == 1);
+        MockFlowFile result = results.get(0);
+        String resultValue = new String(runner.getContentAsByteArray(result));
+
+        // Test attributes and content
+//        result.assertAttributeEquals(CPTADSSDataProviderProcessor.MATCH_ATTR, "nifi rocks");
+        result.assertContentEquals("nifi rocks");       
     }
 
+    @Test
+    public void testProcessorWithOneRequestForDSSFlowFile() 
+    {
+ 
+        // Mock the input file
+        // Add two instruments
+        String ric1 = "2618.TW";
+        String ric2 = "MSFT.OQ";
+        InputStream content = new ByteArrayInputStream("{\"hello\":\"nifi rocks\"}".getBytes());
+
+        // Generate a test runner to mock a processor in a flow
+        TestRunner runner = TestRunners.newTestRunner(new CPTADSSDataProviderProcessor());
+
+        // Set user names, passwords
+        runner.setProperty(CPTADSSDataProviderProcessorConstants.DSS_USER_NAME_PROPERTY, DSS_USER_NAME);
+        runner.setProperty(CPTADSSDataProviderProcessorConstants.DSWS_USER_NAME_PROPERTY, DSWS_USER_NAME);
+        runner.setProperty(CPTADSSDataProviderProcessorConstants.DSS_PASSWORD_PROPERTY, DSS_PASSWORD);
+        runner.setProperty(CPTADSSDataProviderProcessorConstants.DSWS_PASSWORD_PROPERTY, DSWS_PASSWORD);
+
+        // Add the content to the runner
+        runner.enqueue(content);
+
+        // Run the enqueued content, it also takes an int = number of contents queued
+        runner.run(1);
+
+        // All results were processed with out failure
+        runner.assertQueueEmpty();
+
+        // If you need to read or do aditional tests on results you can access the content
+        List<MockFlowFile> results = runner.getFlowFilesForRelationship(CPTADSSDataProviderProcessorConstants.RELATIONSHIP_NAME_SUCCESS);
+        assertTrue("1 match", results.size() == 1);
+        MockFlowFile result = results.get(0);
+        String resultValue = new String(runner.getContentAsByteArray(result));
+
+        // Test attributes and content
+//        result.assertAttributeEquals(CPTADSSDataProviderProcessor.MATCH_ATTR, "nifi rocks");
+        result.assertContentEquals("nifi rocks");       
+    }
+
+    @Test
+    public void testProcessorWithTwoRequestForDSSFlowFile() 
+    {
+ 
+        // Mock the input file
+        // If it is empty then it needs to have
+        InputStream content = new ByteArrayInputStream("{\"hello\":\"nifi rocks\"}".getBytes());
+
+        // Generate a test runner to mock a processor in a flow
+        TestRunner runner = TestRunners.newTestRunner(new CPTADSSDataProviderProcessor());
+
+        // Set user names, passwords
+        runner.setProperty(CPTADSSDataProviderProcessorConstants.DSS_USER_NAME_PROPERTY, DSS_USER_NAME);
+        runner.setProperty(CPTADSSDataProviderProcessorConstants.DSWS_USER_NAME_PROPERTY, DSWS_USER_NAME);
+        runner.setProperty(CPTADSSDataProviderProcessorConstants.DSS_PASSWORD_PROPERTY, DSS_PASSWORD);
+        runner.setProperty(CPTADSSDataProviderProcessorConstants.DSWS_PASSWORD_PROPERTY, DSWS_PASSWORD);
+
+        // Add the content to the runner
+        runner.enqueue(content);
+
+        // Run the enqueued content, it also takes an int = number of contents queued
+        runner.run(1);
+
+        // All results were processed with out failure
+        runner.assertQueueEmpty();
+
+        // If you need to read or do aditional tests on results you can access the content
+        List<MockFlowFile> results = runner.getFlowFilesForRelationship(CPTADSSDataProviderProcessorConstants.RELATIONSHIP_NAME_SUCCESS);
+        assertTrue("1 match", results.size() == 1);
+        MockFlowFile result = results.get(0);
+        String resultValue = new String(runner.getContentAsByteArray(result));
+
+        // Test attributes and content
+//        result.assertAttributeEquals(CPTADSSDataProviderProcessor.MATCH_ATTR, "nifi rocks");
+        result.assertContentEquals("nifi rocks");       
+    }
+
+    @Test
+    public void testProcessorWithOneRequestForDSSAndOneForDSWSFlowFile() 
+    {
+ 
+        // Mock the input file
+        // If it is empty then it needs to have
+        InputStream content = new ByteArrayInputStream("{\"hello\":\"nifi rocks\"}".getBytes());
+
+        // Generate a test runner to mock a processor in a flow
+        TestRunner runner = TestRunners.newTestRunner(new CPTADSSDataProviderProcessor());
+
+        // Set user names, passwords
+        runner.setProperty(CPTADSSDataProviderProcessorConstants.DSS_USER_NAME_PROPERTY, DSS_USER_NAME);
+        runner.setProperty(CPTADSSDataProviderProcessorConstants.DSWS_USER_NAME_PROPERTY, DSWS_USER_NAME);
+        runner.setProperty(CPTADSSDataProviderProcessorConstants.DSS_PASSWORD_PROPERTY, DSS_PASSWORD);
+        runner.setProperty(CPTADSSDataProviderProcessorConstants.DSWS_PASSWORD_PROPERTY, DSWS_PASSWORD);
+
+        // Add the content to the runner
+        runner.enqueue(content);
+
+        // Run the enqueued content, it also takes an int = number of contents queued
+        runner.run(1);
+
+        // All results were processed with out failure
+        runner.assertQueueEmpty();
+
+        // If you need to read or do aditional tests on results you can access the content
+        List<MockFlowFile> results = runner.getFlowFilesForRelationship(CPTADSSDataProviderProcessorConstants.RELATIONSHIP_NAME_SUCCESS);
+        assertTrue("1 match", results.size() == 1);
+        MockFlowFile result = results.get(0);
+        String resultValue = new String(runner.getContentAsByteArray(result));
+
+        // Test attributes and content
+//        result.assertAttributeEquals(CPTADSSDataProviderProcessor.MATCH_ATTR, "nifi rocks");
+        result.assertContentEquals("nifi rocks");       
+    }
+    
+    static String DSS_USER_NAME = "CHANGE_THIS_TO_YOUR_DSS_USER_NAME";
+    static String DSS_PASSWORD = "CHANGE_THIS_TO_YOUR_DSS_PASSWORD";
+    static String DSWS_USER_NAME = "CHANGE_THIS_TO_YOUR_DSWS_USER_NAME";
+    static String DSWS_PASSWORD = "CHANGE_THIS_TO_YOUR_DSWS_PASSWORD";
 }
